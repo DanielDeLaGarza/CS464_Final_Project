c5ee2337583e867cd50b8ae092d94316f925d5a5
JaronShelman <jnshelman@gmail.com>
2015-12-10 10:40:39 -0700

    Red and Blue color additions to game.static.js

--- testProject/js/game/game.static.js
+++ testProject/js/game/game.static.js
@@ -4,16 +4,15 @@
  * This file contains static constants that don't change
  */
 
- //looks like we don't need to make any changes should work well with our new project
-
-
 window.game = window.game || {};
 
 window.game.static = {
-	colors: {
-		black: 0x000000,
-		white: 0xffffff,
-		green: 0x0fdb8c,
-		cyan: 0x38FDD9
-	}
+    colors: {
+        black : 0x000000,
+        white : 0xffffff,
+        green : 0x0fdb8c,
+        cyan  : 0x38FDD9,
+        blue  : 0x00ffff,
+        red   : 0x8b0000,
+    }
 };
