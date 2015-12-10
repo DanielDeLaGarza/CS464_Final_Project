3235dc87031ddb5a20d9918afa5561d58c47f345
Daniel De La Garza <josedelagarza@u.boisestate.edu>
2015-12-10 09:55:01 -0700

    added comments

---
+++ testProject/js/game/game.static.js
@@ -0,0 +1,19 @@
+/*
+ * Game Constants
+ *
+ * This file contains static constants that don't change
+ */
+
+ //looks like we don't need to make any changes should work well with our new project
+
+
+window.game = window.game || {};
+
+window.game.static = {
+	colors: {
+		black: 0x000000,
+		white: 0xffffff,
+		green: 0x0fdb8c,
+		cyan: 0x38FDD9
+	}
+};
